17d2a01a8665c71d2b95a46e06c31cff0015b226
mahmoudelnagar5 <ma982@gmail.com>
2024-09-24 17:25:04 +0300

    actice all expensess item part two

--- lib/widgets/all_expenses_item_header.dart
+++ lib/widgets/all_expenses_item_header.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class AllExpensesItemHeader extends StatelessWidget {
-  const AllExpensesItemHeader({super.key, required this.image});
+  const AllExpensesItemHeader(
+      {super.key,
+      required this.image,
+      this.imageBackgroundColor,
+      this.imageColor});
   final String image;
+  final Color? imageBackgroundColor, imageColor;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -12,18 +17,22 @@ class AllExpensesItemHeader extends StatelessWidget {
           width: 60,
           height: 60,
           padding: const EdgeInsets.all(14),
-          decoration: const ShapeDecoration(
-            color: Color(0xFFFAFAFA),
-            shape: OvalBorder(),
+          decoration: ShapeDecoration(
+            color: imageBackgroundColor ?? const Color(0xFFFAFAFA),
+            shape: const OvalBorder(),
           ),
-          child: SvgPicture.asset(image),
+          child: SvgPicture.asset(
+            image,
+            colorFilter: ColorFilter.mode(
+                imageColor ?? const Color(0xFF4EB7F2), BlendMode.srcIn),
+          ),
         ),
         const Spacer(),
         Transform.rotate(
           angle: -1.57079633 * 2,
-          child: const Icon(
+          child: Icon(
             Icons.arrow_back_ios_new_outlined,
-            color: Color(0xFF064061),
+            color: imageColor == null ? const Color(0xFF064061) : Colors.white,
           ),
         ),
       ],
--- lib/widgets/inactive_and_active_all_expenses_item.dart
+++ lib/widgets/inactive_and_active_all_expenses_item.dart
@@ -72,12 +72,8 @@ class ActiveAllExpensesItem extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
       decoration: ShapeDecoration(
-        color: Colors.white,
+        color: const Color(0XFF4DB7F2),
         shape: RoundedRectangleBorder(
-          side: const BorderSide(
-            width: 1,
-            color: Color(0xFFF1F1F1),
-          ),
           borderRadius: BorderRadius.circular(12),
         ),
       ),
@@ -85,6 +81,8 @@ class ActiveAllExpensesItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AllExpensesItemHeader(
+            imageBackgroundColor: Colors.white.withOpacity(0.10000000149011612),
+            imageColor: Colors.white,
             image: itemModel.image,
           ),
           const SizedBox(
@@ -92,21 +90,27 @@ class ActiveAllExpensesItem extends StatelessWidget {
           ),
           Text(
             itemModel.title,
-            style: AppStyles.styleMedium16,
+            style: AppStyles.styleMedium16.copyWith(
+              color: Colors.white,
+            ),
           ),
           const SizedBox(
             height: 8,
           ),
           Text(
             itemModel.date,
-            style: AppStyles.styleRegular14,
+            style: AppStyles.styleRegular14.copyWith(
+              color: const Color(0xFFFAFAFA),
+            ),
           ),
           const SizedBox(
             height: 16,
           ),
           Text(
             itemModel.price,
-            style: AppStyles.styleSemiBold24,
+            style: AppStyles.styleSemiBold24.copyWith(
+              color: Colors.white,
+            ),
           ),
         ],
       ),

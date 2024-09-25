08f004c7b94d63aab806d7d27086fa14d5053f1b
mahmoudelnagar5 <ma982@gmail.com>
2024-09-25 16:34:42 +0300

    transction history part one

---
+++ lib/widgets/transction_history.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class TransctionHistory extends StatelessWidget {
+  const TransctionHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Transaction History',
+              style: AppStyles.styleSemiBold20,
+            ),
+            Text(
+              'See All',
+              style: AppStyles.styleMedium16.copyWith(
+                color: const Color(0xFF4EB7F2),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}

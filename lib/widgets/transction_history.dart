dbccd210c91dafcaeb6769b9686251fb5ad9d127
mahmoudelnagar5 <ma982@gmail.com>
2024-09-25 16:38:59 +0300

    transction history part two

--- lib/widgets/transction_history.dart
+++ lib/widgets/transction_history.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/transction_history_header.dart';
 
 class TransctionHistory extends StatelessWidget {
   const TransctionHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(
-              'Transaction History',
-              style: AppStyles.styleSemiBold20,
-            ),
-            Text(
-              'See All',
-              style: AppStyles.styleMedium16.copyWith(
-                color: const Color(0xFF4EB7F2),
-              ),
-            ),
-          ],
+        TransctionHistoryHeader(),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          '13 April 2022',
+          style: TextStyle(
+            color: Color(0xFFAAAAAA),
+            fontSize: 16,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w500,
+            height: 0,
+          ),
         )
       ],
     );

65146554a14582478b8daa7bfa7075d36c53b1de
mahmoudelnagar5 <ma982@gmail.com>
2024-09-26 17:32:37 +0300

    resposnive tablet layout

--- lib/widgets/active_and_inactive_item.dart
+++ lib/widgets/active_and_inactive_item.dart
@@ -16,9 +16,13 @@ class ActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.icon),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleBold16(context),
+      title: FittedBox(
+        fit: BoxFit.scaleDown,
+        alignment: AlignmentDirectional.centerStart,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleBold16(context),
+        ),
       ),
       trailing: Container(
         decoration: const BoxDecoration(
@@ -42,9 +46,13 @@ class InActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.icon),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleMedium16(context),
+      title: FittedBox(
+        alignment: AlignmentDirectional.centerStart,
+        fit: BoxFit.scaleDown,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleMedium16(context),
+        ),
       ),
     );
   }

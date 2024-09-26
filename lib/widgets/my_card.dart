057fcd3e7eed20751ddb166c757b0fcafc2325ab
mahmoudelnagar5 <ma982@gmail.com>
2024-09-26 17:14:34 +0300

    resposnvie desktop layout done

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -58,8 +58,10 @@ class MyCard extends StatelessWidget {
                 ],
               ),
             ),
-            const SizedBox(
-              height: 54 - 28,
+            const Flexible(
+              child: SizedBox(
+                height: 54 - 28,
+              ),
             ),
           ],
         ),
